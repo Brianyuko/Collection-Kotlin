79c1271793bec3d4fb771b114e381a9cc39457da
Brianyuko <brianyuko45@gmail.com>
2020-05-14 15:26:34 +0700

    Intro Collection

---
+++ src/Collection.kt
@@ -0,0 +1,10 @@
+fun main (){
+
+    // Array -> Fixed Size, Mutable
+
+    // Collections
+        // 1. Immutable -> Tidak Bisa diubah
+        // 2. Mutable -> Bisa diubah
+
+            // List, Map, Set
+}
